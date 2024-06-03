3c55f561a7552ff1f86f4a49d8b446512fc13bda
Peter Gergel <peter.gergel@fr24.com>
2024-06-03 15:33:07 +0200

    ignoring files

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,23 +1,28 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
-vim.keymap.set('n', '<leader>fr', builtin.lsp_references, {})
-vim.keymap.set('n', '<leader>rr', builtin.resume, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
+vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
+vim.keymap.set("n", "<leader>rr", builtin.resume, {})
 
 builtin.buffers({ sort_lastused = true, ignore_current_buffer = true })
 
-require('telescope').setup{
-    pickers = {
-      live_grep = {
-        theme = "dropdown",
-        layout_config = {
-            center = {
-                width = 0.9,
-                height = 0.5,
-            },
-        },
-      }
-    },
-}
+require("telescope").setup({
+	defaults = {
+		file_ignore_patterns = {
+			"compile_commands.json",
+		},
+	},
+	pickers = {
+		live_grep = {
+			theme = "dropdown",
+			layout_config = {
+				center = {
+					width = 0.9,
+					height = 0.5,
+				},
+			},
+		},
+	},
+})
